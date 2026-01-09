6df31ecd97b8a53fcafd86d0397ea9fc8234a8b0
solsikke74 <117269718+solsikke74@users.noreply.github.com>
2026-01-09 08:56:15 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+
+const x = document.getElementById("demo");
+
+function getLocation() {
+  try {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } catch(err) {
+    x.innerHTML = err;
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
+}
