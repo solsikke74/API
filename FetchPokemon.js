dc976be2ab589cec79a4b25fd005b15c61f15328
solsikke74 <117269718+solsikke74@users.noreply.github.com>
2026-01-09 14:24:04 +0100

    Update FetchPokemon.js

---
+++ FetchPokemon.js
@@ -0,0 +1,15 @@
+//fetch funcion used for making http requests to fetch resources, 
+//(JSON style data, images, files)
+//Simplifies asynchronous data fetching in Javascript and used for interacting with APIs 
+//to retrieve and send data asynchronously over the web. 
+//fetch(url, {options})
+
+async function fetchData(){
+    try{
+        const PokemonName = document.elementById("PokeomName").value
+        const response = await fetch 
+
+    }
+
+
+}
